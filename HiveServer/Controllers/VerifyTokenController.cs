c42ca002dcdd891fae1803ab4bfacf06534b32ad
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-07-15 12:24:49 +0900

    FIX Controller name

---
+++ HiveServer/Controllers/VerifyTokenController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using HiveServer.Services.Interfaces;
+using HiveServer.DTO;
+
+namespace HiveServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VerifyTokenController : ControllerBase
+{
+    private readonly ILogger<VerifyTokenController> _logger;
+    private readonly IVerifyTokenService _verifyTokenService;
+
+    public VerifyTokenController(ILogger<VerifyTokenController> logger, IVerifyTokenService verifyTokenService)
+    {
+        _logger = logger;
+        _verifyTokenService = verifyTokenService;
+    }
+
+    [HttpPost]
+    public async Task<VerifyTokenResponse> Verify([FromBody] VerifyTokenRequest request)
+    {
+        var response = await _verifyTokenService.Verify(request);
+        _logger.LogInformation($"[VerifyToken] hive_player_id: {request.hive_player_id}, Result: {response.Result}");
+        return response;
+    }
+}
