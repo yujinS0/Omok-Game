7b4c57eec6ee08dddb3bacb3c01da5471eedc931
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-07-17 15:47:08 +0900

    ADD ServerShared

---
+++ ServerShared/KeyGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerShared;
+
+public class KeyGenerator
+{
+    public static string GenerateMatchResultKey(string playerId)
+    {
+        return $"M_{playerId}_Result";
+    }
+
+    public static string GeneratePlayingUserKey(string playerId)
+    {
+        return $"P_{playerId}_Info";
+    }
+    public static string GenerateUserLoginKey(string playerId)
+    {
+        return $"U_{playerId}_Login";
+    }
+
+    public static string GenerateGameRoomId()
+    {
+        return Guid.NewGuid().ToString(); // [TODO] Ulid 확인 후, 바꾸기
+    }
+}
---
+++ ServerShared/Omok.cs
@@ -0,0 +1,246 @@
+﻿using System.Text;
+
+namespace ServerShared;
+
+public enum OmokStone
+{
+    None,
+    Black,
+    White
+}
+
+//1.오목 OmokGameData 초기값 설정
+// OmokGameData는 바이너리 배열 형태로 현재 게임 정보를 Redis에 저장하기 위한 것
+// 매칭 성사 시에 MakeRawData로 데이터를 생성한다
+// 데이터 구조 : 오목판 정보 + 흑돌 유저이름 + 백돌 유저이름 + 현재 턴(어떤 돌의 차례인지 1인지 2인지) + 턴 시간(최근 돌 두기 요청이 온 시간) + 이긴 사람 정보(없으면 0, 흑돌 이기면 1, 백돌 이기면 2)
+// 돌에 대해서 None이면 0, 흑돌이면 1, 백돌이면 2로 고정한다.
+// 초기 값 : 오목판은 모두 0, 현재 턴도 처음 생성 시에는 0, 턴 시간 초기값도 현재 데이터 생성한 시간 + 이긴 사람 정보 0
+
+
+// 2. 게임 시작 StartGame() 함수
+// StartGame() 함수가 호출된다면 
+// -> 초기값에서 현재 턴을 1(흑돌)로 바꾸고 턴시간을 해당 함수 호출 시점으로 바꾼다.
+
+
+// 3. 돌두기 SetStone() 함수
+// SetStone() 즉 돌두기 함수가 호출된다면
+// -> 초기값에서 오목판 정보를 입력받은 x, y값을 가지고 바꾸기. 현재턴을 다음 돌로 바꾸고 (isBlack이 True면 흑돌이라는 뜻이기 때문에 이제 백돌 차례라서 2로 바꾸기, False라면 1로 바꾸기), 턴시간도 현재 함수 호출 시간으로 바꾼다.
+
+public class OmokGameData
+{
+    const int BoardSize = 15;
+    const int BoardSizeSquare = BoardSize * BoardSize;
+
+    const byte BlackStone = 1;
+    const byte WhiteStone = 2;
+
+    // 오목판 정보 BoardSize * BoardSize
+    // 블랙 플레이어의 이름: 1(이름 바이트 수) + N(앞에서 구한 길이)
+    // 화이트 플레이어의 이름: 1(이름 바이트 수) + N(앞에서 구한 길이)
+    byte[] _rawData;
+
+    string _blackPlayer;
+    string _whitePlayer;
+
+
+    OmokStone _turnPlayerStone; // 턴 받은 플레이어
+    UInt64 _turnTimeMilli; // 턴 받은 시간 유닉스 시간(초)
+    OmokStone _winner;
+
+
+    public byte[] GetRawData()
+    {
+        return _rawData;
+    }
+
+    public byte[] MakeRawData(string blackPlayer, string whitePlayer)  // rawDataSize를 따로 입력받는 것이 아니라 이름 길이에 따라 동적으로 변경하도록 수정했습니다.
+    {
+        // 플레이어 이름의 길이를 동적으로 계산
+        var blackPlayerBytes = Encoding.UTF8.GetBytes(blackPlayer);
+        var whitePlayerBytes = Encoding.UTF8.GetBytes(whitePlayer);
+
+        // 데이터 크기 계산
+        int rawDataSize = BoardSizeSquare + // 오목판 정보
+                          1 + blackPlayerBytes.Length + // 흑돌 플레이어 이름 (길이 1 바이트 + 실제 이름 데이터)
+                          1 + whitePlayerBytes.Length + // 백돌 플레이어 이름 (길이 1 바이트 + 실제 이름 데이터)
+                          1 + // 현재 턴 정보
+                          8 + // 턴 시작 시각 (돌 둔 시간)
+                          1;  // 이긴 사람 정보
+
+        var rawData = new byte[rawDataSize];
+        var index = 0;
+
+        // 1. 오목판 정보 초기화 (모두 0)
+        for (int i = 0; i < BoardSizeSquare; i++)
+        {
+            rawData[index++] = (byte)OmokStone.None;
+        }
+
+        // 2. 흑돌 플레이어 이름 저장
+        rawData[index++] = (byte)blackPlayerBytes.Length;
+        Array.Copy(blackPlayerBytes, 0, rawData, index, blackPlayerBytes.Length);
+        index += blackPlayerBytes.Length;
+
+        // 3. 백돌 플레이어 이름 저장
+        rawData[index++] = (byte)whitePlayerBytes.Length;
+        Array.Copy(whitePlayerBytes, 0, rawData, index, whitePlayerBytes.Length);
+        index += whitePlayerBytes.Length;
+
+        // 4. 현재 턴 정보 저장 (초기값 0)
+        rawData[index++] = (byte)OmokStone.None;
+
+        // 5. 턴 시간 저장 (초기값 현재 시간)
+        var turnTime = BitConverter.GetBytes((UInt64)DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+        Array.Copy(turnTime, 0, rawData, index, turnTime.Length);
+        index += turnTime.Length;
+
+        // 6. 이긴 사람 정보 저장 (초기값 0)
+        rawData[index++] = (byte)OmokStone.None;
+
+        return rawData;
+    }
+
+    public OmokStone GetStoneAt(int x, int y) // 좌표의 돌 색
+    {
+        int index = y * BoardSize + x;
+        return (OmokStone)_rawData[index];
+    }
+
+    public string GetBlackPlayerName() // 흑돌 플레이어 이름
+    {
+        int index = BoardSizeSquare;
+        int length = _rawData[index];
+        index += 1;
+        return Encoding.UTF8.GetString(_rawData, index, length);
+    }
+
+    public string GetWhitePlayerName() // 백돌 플레이어 이름
+    {
+        int index = BoardSizeSquare;
+        int blackPlayerNameLength = _rawData[index];
+        index += 1 + blackPlayerNameLength;
+        int whitePlayerNameLength = _rawData[index];
+        index += 1;
+        return Encoding.UTF8.GetString(_rawData, index, whitePlayerNameLength);
+    }
+
+    public OmokStone GetCurrentTurn() // 현재 턴 정보 반환
+    {
+        int index = BoardSizeSquare + 1 + GetBlackPlayerName().Length + 1 + GetWhitePlayerName().Length;
+        return (OmokStone)_rawData[index];
+    }
+
+    public UInt64 GetTurnTime() // 현재 턴 시작 시각 반환
+    {
+        int index = BoardSizeSquare + 1 + GetBlackPlayerName().Length + 1 + GetWhitePlayerName().Length + 1;
+        return BitConverter.ToUInt64(_rawData, index);
+    }
+
+    public OmokStone GetWinner() // 이긴 사람 정보 반환
+    {
+        int index = BoardSizeSquare + 1 + GetBlackPlayerName().Length + 1 + GetWhitePlayerName().Length + 1 + 8;
+        return (OmokStone)_rawData[index];
+    }
+
+
+    public void Decoding(byte[] rawData)
+    {
+        _rawData = rawData;
+
+        DecodingUserName();
+        DecodingTurnAndTime();
+        DecodingWinner();
+    }
+
+    public byte[] StartGame(byte[] rawData)
+    {
+        Decoding(rawData);
+        _turnPlayerStone = OmokStone.Black;
+        _turnTimeMilli = (UInt64)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        int turnIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length;
+        rawData[turnIndex] = (byte)OmokStone.Black;
+
+        var turnTimeBytes = BitConverter.GetBytes(_turnTimeMilli);
+        Array.Copy(turnTimeBytes, 0, rawData, turnIndex + 1, turnTimeBytes.Length);
+
+        return rawData;
+    }
+
+    //public void StartGame()
+    //{
+    //    _turnPlayerStone = OmokStone.Black;
+    //    _turnTimeMilli = (UInt64)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    //    int turnIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length;
+    //    _rawData[turnIndex] = (byte)OmokStone.Black;
+
+    //    var turnTimeBytes = BitConverter.GetBytes(_turnTimeMilli);
+    //    Array.Copy(turnTimeBytes, 0, _rawData, turnIndex + 1, turnTimeBytes.Length);
+    //}
+
+    public byte[] SetStone(byte[] rawData, bool isBlack, int x, int y)
+    {
+        Decoding(rawData);
+        int index = y * BoardSize + x;
+        rawData[index] = (byte)(isBlack ? OmokStone.Black : OmokStone.White);
+
+        _turnPlayerStone = isBlack ? OmokStone.White : OmokStone.Black;
+        _turnTimeMilli = (UInt64)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        int turnIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length;
+        rawData[turnIndex] = (byte)_turnPlayerStone;
+
+        var turnTimeBytes = BitConverter.GetBytes(_turnTimeMilli);
+        Array.Copy(turnTimeBytes, 0, rawData, turnIndex + 1, turnTimeBytes.Length);
+
+        return rawData;
+    }
+
+    //public void SetStone(bool isBlack, int x, int y)
+    //{
+    //    int index = y * BoardSize + x;
+    //    _rawData[index] = (byte)(isBlack ? OmokStone.Black : OmokStone.White);
+
+    //    _turnPlayerStone = isBlack ? OmokStone.White : OmokStone.Black;
+    //    _turnTimeMilli = (UInt64)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    //    int turnIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length;
+    //    _rawData[turnIndex] = (byte)_turnPlayerStone;
+
+    //    var turnTimeBytes = BitConverter.GetBytes(_turnTimeMilli);
+    //    Array.Copy(turnTimeBytes, 0, _rawData, turnIndex + 1, turnTimeBytes.Length);
+    //}
+
+    public void OmokCheck()
+    {
+
+    }
+
+
+    void DecodingUserName()
+    {
+        var index = BoardSizeSquare;
+
+        int blackPlayerNameLength = _rawData[index];
+        index += 1;
+        _blackPlayer = Encoding.UTF8.GetString(_rawData, index, blackPlayerNameLength);
+
+        index += blackPlayerNameLength;
+        int whitePlayerNameLength = _rawData[index];
+        index += 1;
+        _whitePlayer = Encoding.UTF8.GetString(_rawData, index, whitePlayerNameLength);
+    }
+
+    void DecodingTurnAndTime()
+    {
+        int turnIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length;
+        _turnPlayerStone = (OmokStone)_rawData[turnIndex];
+
+        var turnTimeBytes = new byte[8];
+        Array.Copy(_rawData, turnIndex + 1, turnTimeBytes, 0, 8);
+        _turnTimeMilli = BitConverter.ToUInt64(turnTimeBytes, 0);
+    }
+
+    void DecodingWinner()
+    {
+        int winnerIndex = BoardSizeSquare + 1 + _blackPlayer.Length + 1 + _whitePlayer.Length + 1 + 8;
+        _winner = (OmokStone)_rawData[winnerIndex];
+    }
+}
