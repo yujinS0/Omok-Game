602e10042f3533ee2ce6363f40b5c86cb015d227
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-07-19 15:37:03 +0900

    ADD omok game info DTO

---
+++ GameServer/DTO/OmokGame.cs
@@ -0,0 +1,51 @@
+﻿using ServerShared;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameServer.DTO;
+
+public class PutOmokRequest
+{
+    [Required] public string PlayerId { get; set; }
+    [Required] public int X { get; set; }
+    [Required] public int Y { get; set; }
+}
+
+public class PutOmokResponse
+{
+    [Required] public ErrorCode Result { get; set; } = ErrorCode.None;
+}
+
+public class PlayerRequest
+{
+    public string PlayerId { get; set; }
+}
+
+public class BoardResponse
+{
+    public ErrorCode Result { get; set; }
+    public byte[] Board { get; set; }
+}
+
+public class PlayerResponse
+{
+    public ErrorCode Result { get; set; }
+    public string PlayerId { get; set; }
+}
+
+public class CurrentTurnResponse
+{
+    public ErrorCode Result { get; set; }
+    public OmokStone CurrentTurn { get; set; }
+}
+
+public class WinnerResponse
+{
+    public ErrorCode Result { get; set; }
+    public Winner Winner { get; set; }
+}
+
+public class Winner
+{
+    public OmokStone Stone { get; set; }
+    public string PlayerId { get; set; }
+}
