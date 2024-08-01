6420dd2bff66b6f537f9e0394cc5e9144542e2dd
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-08-01 18:25:14 +0900

    사용하지 않는 TODO 삭제

--- GameServer/GameConstants.cs
+++ GameServer/GameConstants.cs
@@ -1,7 +1,6 @@
 ﻿namespace GameServer;
 
-//TODO: (08.01) 사용하지 않는 //TODO는 삭제해주세요
-public class GameConstants // TODO MasterDB 사용
+public class GameConstants
 {
     public const int WinExp = 20;
     public const int LoseExp = 10;
