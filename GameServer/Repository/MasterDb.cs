158624577e7bb40ff4239b6e4837275a4fe0ced0
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-08-05 13:01:21 +0900

    Refactor : 가독성을 위해 MasterDb.Load() 함수 분리

--- GameServer/Repository/MasterDb.cs
+++ GameServer/Repository/MasterDb.cs
@@ -25,7 +25,6 @@ public class MasterDb : IMasterDb
     {
         _logger = logger;
         _dbConfig = dbConfig;
-
         
         var loadTask = Load();
         loadTask.Wait();
@@ -35,12 +34,11 @@ public class MasterDb : IMasterDb
             throw new InvalidOperationException("Failed to load master data from the database. Server is shutting down.");
         }
     }
-        
 
     public async Task<bool> Load()
     {
         //TODO: (08.05) 함수가 너무 큽니다. 적절하게 분리해주세요
-
+        //=> 수정 완료했습니다
         MySqlConnection connection = null;
         try
         {
@@ -48,84 +46,13 @@ public class MasterDb : IMasterDb
             connection.Open();
 
             var queryFactory = new QueryFactory(connection, new MySqlCompiler());
-
-            // Load Version
-            var getVersionResult = await queryFactory.Query("version").FirstOrDefaultAsync();
-            if (getVersionResult == null)
+            
+            if (!await LoadVersion(queryFactory) ||
+                !await LoadAttendanceRewards(queryFactory) ||
+                !await LoadItems(queryFactory) ||
+                !await LoadFirstItems(queryFactory))
             {
-                _logger.LogWarning("No Version data found [MasterDb]");
                 return false;
-            }
-
-            var version = new GameServer.Models.Version
-            {
-                AppVersion = getVersionResult.app_version,
-                MasterDataVersion = getVersionResult.master_data_version
-            };
-            _version = version;
-
-            _logger.LogInformation($"Loaded version: AppVersion={_version.AppVersion}, MasterDataVersion={_version.MasterDataVersion}");
-
-            // Load AttendanceReward
-            var attendanceRewardsResult = await queryFactory.Query("attendance_reward").GetAsync();
-            if (attendanceRewardsResult == null || !attendanceRewardsResult.Any())
-            {
-                _logger.LogWarning("No AttendanceReward data found [MasterDb]");
-                return false;
-            }
-
-            _attendanceRewardList = attendanceRewardsResult.Select(ar => new AttendanceReward
-            {
-                DaySeq = ar.day_seq,
-                RewardItem = ar.reward_item,
-                ItemCount = ar.item_count
-            }).ToList();
-
-            _logger.LogInformation("Loaded attendance rewards:");
-            foreach (var reward in _attendanceRewardList)
-            {
-                _logger.LogInformation($"DaySeq={reward.DaySeq}, RewardItem={reward.RewardItem}, ItemCount={reward.ItemCount}");
-            }
-
-            // Load Item
-            var itemsResult = await queryFactory.Query("item").GetAsync();
-            if (itemsResult == null || !itemsResult.Any())
-            {
-                _logger.LogWarning("No Item data found [MasterDb]");
-                return false;
-            }
-
-            _itemList = itemsResult.Select(it => new Item
-            {
-                ItemCode = it.item_code,
-                Name = it.name,
-                Description = it.description
-            }).ToList();
-
-            _logger.LogInformation("Loaded items:");
-            foreach (var item in _itemList)
-            {
-                _logger.LogInformation($"ItemCode={item.ItemCode}, Name={item.Name}, Description={item.Description}");
-            }
-
-            // Load FirstItem
-            var firstItemsResult = await queryFactory.Query("first_item").GetAsync();
-            if (firstItemsResult == null || !firstItemsResult.Any())
-            {
-                _logger.LogWarning("No FirstItem data found [MasterDb]");
-                return false;
-            }
-
-            _firstItemList = firstItemsResult.Select(fi => new FirstItem
-            {
-                ItemCode = fi.item_code,
-                Count = fi.count
-            }).ToList();
-
-            _logger.LogInformation("Loaded first items:");
-            foreach (var firstItem in _firstItemList)
-            {
-                _logger.LogInformation($"ItemCode={firstItem.ItemCode}, Count={firstItem.Count}");
             }
         }
         catch (Exception e)
@@ -135,14 +62,102 @@ public class MasterDb : IMasterDb
         }
         finally
         {
-            // Load 메서드가 끝나면 항상 연결을 닫습니다.
             if (connection != null)
             {
                 await connection.CloseAsync();
                 await connection.DisposeAsync();
             }
         }
+        return true;
+    }
 
+    private async Task<bool> LoadVersion(QueryFactory queryFactory)
+    {
+        var getVersionResult = await queryFactory.Query("version").FirstOrDefaultAsync();
+        if (getVersionResult == null)
+        {
+            _logger.LogWarning("No Version data found [MasterDb]");
+            return false;
+        }
+
+        _version = new GameServer.Models.Version
+        {
+            AppVersion = getVersionResult.app_version,
+            MasterDataVersion = getVersionResult.master_data_version
+        };
+
+        _logger.LogInformation($"Loaded version: AppVersion={_version.AppVersion}, MasterDataVersion={_version.MasterDataVersion}");
+        return true;
+    }
+
+    private async Task<bool> LoadAttendanceRewards(QueryFactory queryFactory)
+    {
+        var attendanceRewardsResult = await queryFactory.Query("attendance_reward").GetAsync();
+        if (attendanceRewardsResult == null || !attendanceRewardsResult.Any())
+        {
+            _logger.LogWarning("No AttendanceReward data found [MasterDb]");
+            return false;
+        }
+
+        _attendanceRewardList = attendanceRewardsResult.Select(ar => new AttendanceReward
+        {
+            DaySeq = ar.day_seq,
+            RewardItem = ar.reward_item,
+            ItemCount = ar.item_count
+        }).ToList();
+
+        _logger.LogInformation("Loaded attendance rewards:");
+        foreach (var reward in _attendanceRewardList)
+        {
+            _logger.LogInformation($"DaySeq={reward.DaySeq}, RewardItem={reward.RewardItem}, ItemCount={reward.ItemCount}");
+        }
+        return true;
+    }
+
+    private async Task<bool> LoadItems(QueryFactory queryFactory)
+    {
+        var itemsResult = await queryFactory.Query("item").GetAsync();
+        if (itemsResult == null || !itemsResult.Any())
+        {
+            _logger.LogWarning("No Item data found [MasterDb]");
+            return false;
+        }
+
+        _itemList = itemsResult.Select(it => new Item
+        {
+            ItemCode = it.item_code,
+            Name = it.name,
+            Description = it.description
+        }).ToList();
+
+        _logger.LogInformation("Loaded items:");
+        foreach (var item in _itemList)
+        {
+            _logger.LogInformation($"ItemCode={item.ItemCode}, Name={item.Name}, Description={item.Description}");
+        }
+        return true;
+    }
+
+    private async Task<bool> LoadFirstItems(QueryFactory queryFactory)
+    {
+        var firstItemsResult = await queryFactory.Query("first_item").GetAsync();
+        if (firstItemsResult == null || !firstItemsResult.Any())
+        {
+            _logger.LogWarning("No FirstItem data found [MasterDb]");
+            return false;
+        }
+
+        _firstItemList = firstItemsResult.Select(fi => new FirstItem
+        {
+            ItemCode = fi.item_code,
+            Count = fi.count
+        }).ToList();
+
+        _logger.LogInformation("Loaded first items:");
+        foreach (var firstItem in _firstItemList)
+        {
+            _logger.LogInformation($"ItemCode={firstItem.ItemCode}, Count={firstItem.Count}");
+        }
         return true;
     }
 
