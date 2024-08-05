0a62de8c9289f786a8a0457b6b7a5d13fe9b969b
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-08-05 19:33:34 +0900

    ADD MailBoxList 와 생성자

--- GameServer/Models/GameDb.cs
+++ GameServer/Models/GameDb.cs
@@ -11,3 +11,21 @@ public class PlayerInfo
     public int Lose { get; set; }
     public int Draw { get; set; }
 }
+
+public class MailBoxList
+{
+    public List<long> MailIds { get; set; }
+    public List<string> MailTitles { get; set; }
+    public List<int> ItemCodes { get; set; }
+    public List<DateTime> SendDates { get; set; }
+    public List<int> ReceiveYns { get; set; }
+
+    public MailBoxList()
+    {
+        MailIds = new List<long>();
+        MailTitles = new List<string>();
+        ItemCodes = new List<int>();
+        SendDates = new List<DateTime>();
+        ReceiveYns = new List<int>();
+    }
+}
