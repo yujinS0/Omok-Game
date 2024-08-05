33306cc8362c841f735d258e3a261dd1d4327820
yujinS0 <COM2US-SUMMER9@930YUJIN>
2024-08-05 12:39:08 +0900

    FIX MagicNumber 제거를 위한 Const 추가

--- GameServer/GameConstants.cs
+++ GameServer/GameConstants.cs
@@ -4,4 +4,7 @@ public class GameConstants
 {
     public const int WinExp = 20;
     public const int LoseExp = 10;
+
+    public const int GameMoneyItemCode = 1;
+    public const int DiamondItemCode = 2;
 }
