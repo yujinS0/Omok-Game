ec7253d2834b8417230dc8c0e7b1a303198817fa
U_Zin <930yujin@gmail.com>
2024-08-08 12:58:05 +0900

    ADD Friend API controller

---
+++ GameServer/Controllers/FriendController.cs
@@ -0,0 +1,75 @@
+﻿using GameServer.DTO;
+using GameServer.Models;
+using GameServer.Services;
+using GameServer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using ServerShared;
+
+namespace GameServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FriendController : ControllerBase
+{
+    private readonly ILogger<FriendController> _logger;
+    private readonly IFriendService _friendService;
+
+    public FriendController(ILogger<FriendController> logger, IFriendService friendService)
+    {
+        _logger = logger;
+        _friendService = friendService;
+    }
+
+    [HttpPost("get-list")]
+    public async Task<GetFriendListResponse> GetFriendList([FromBody] GetFriendListRequest request)
+    {
+        var playerUid = (long)HttpContext.Items["PlayerUid"];
+        (ErrorCode result, List<string> friendNickNames, List<DateTime> createDt) = await _friendService.GetFriendList(playerUid);
+
+        return new GetFriendListResponse
+        {
+            Result = result,
+            FriendNickNames = friendNickNames,
+            CreateDt = createDt
+        };
+    }
+
+    [HttpPost("get-request-list")]
+    public async Task<GetFriendRequestListResponse> GetFriendRequestList([FromBody] GetFriendRequestListRequest request)
+    {
+        var playerUid = (long)HttpContext.Items["PlayerUid"];
+        (ErrorCode result, List<string> reqFriendNickNames, List<int> state, List<DateTime> createDt) = await _friendService.GetFriendRequestList(playerUid);
+
+        return new GetFriendRequestListResponse
+        {
+            Result = result,
+            ReqFriendNickNames = reqFriendNickNames,
+            State = state,
+            CreateDt = createDt
+        };
+    }
+
+    [HttpPost("request")]
+    public async Task<RequestFriendResponse> RequestFriend([FromBody] RequestFriendRequest request)
+    {
+        var playerUid = (long)HttpContext.Items["PlayerUid"];
+        var result = await _friendService.RequestFriend(playerUid, request.FriendPlayerId);
+
+        return new RequestFriendResponse
+        {
+            Result = result
+        };
+    }
+
+    [HttpPost("accept")]
+    public async Task<AcceptFriendResponse> AcceptFriend([FromBody] AcceptFriendRequest request)
+    {
+        var playerUid = (long)HttpContext.Items["PlayerUid"];
+        var result = await _friendService.AcceptFriend(playerUid, request.FriendPlayerId);
+
+        return new AcceptFriendResponse
+        {
+            Result = result
+        };
+    }
+}
