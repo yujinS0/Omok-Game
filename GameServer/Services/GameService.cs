c68671826508b4a78121d98fd4566d775520201a
heungbae <heungabe@com2us.com>
2024-09-20 15:22:06 +0900

    주석 삭제

--- GameServer/Services/GameService.cs
+++ GameServer/Services/GameService.cs
@@ -32,11 +32,6 @@ public class GameService : IGameService
             return (validatePlayerTurn, null);
         }
 
-        //TODO: (08.08) 아래에서 예외가 발생할 일이 있을까요? 이미 레포지트에서 예외처리는 다 하고 있을테니
-        //  다른 코드에도 이런 것이 보이는데 불필요하게 에외문을 사용하지는 마세요
-        //=> 진행중 다른 부분 코드 확인중
-
-        //=> 바로 아래부분은 수정 완료했습니다.
         omokGameData.SetStone(playerId, x, y);
         bool updateResult = await _memoryDb.UpdateGameData(gameRoomId, omokGameData.GetRawData());
 
@@ -46,8 +41,6 @@ public class GameService : IGameService
             return (ErrorCode.UpdateGameDataFailException, null);
         }
 
-        //TODO: (08.08) 게임이 끝난 경우라면 GameRoomId 으로 만들어지는 두명의 플레이의 키를 저장한 데이터도 게임이 끝났을 때의 UpdateGameData에서 사용하는 expire 시간과 동일한 시간내에서 삭제되도록 해야합니다.
-        // 이렇게 해야 게임이 끝났는데도 계속 데이터를 요청하는 어뷰징을 막을 수 있습니다
         var (result, winner) = await CheckForWinner(omokGameData);
         if (result != ErrorCode.None)
         {
@@ -59,10 +52,6 @@ public class GameService : IGameService
 
     private async Task<(ErrorCode, OmokGameEngine, string)> ValidatePlayerTurn(string playerId)
     {
-        //TODO: (08.08) 코드 가독성을 위해 아래 코드들을 함수로 분리하도록 하시죠
-
-        //TODO: (08.08) var gameRoomId = await _memoryDb.GetGameRoomId(playerId);와 중복코드 아닌가요?
-        //=> 맞습니다 수정 완료했습니다.
         string gameRoomId = await _memoryDb.GetGameRoomId(playerId);
 
         if (gameRoomId == null)
